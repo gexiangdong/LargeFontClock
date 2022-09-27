84b3110009302799b7e7916af01268fa9d551131
gexiangdong <gexiangdong@gmail.com>
2022-09-27 09:55:30 +0800

    Initial Commit

---
+++ LargeFontClock/LargeFontClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LargeFontClockApp.swift
+//  LargeFontClock
+//
+//  Created by GeXiangDong on 2022/9/27.
+//
+
+import SwiftUI
+
+@main
+struct LargeFontClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
